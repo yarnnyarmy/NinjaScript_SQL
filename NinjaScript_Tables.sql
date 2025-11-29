d24920da745d1a70095e681cccbad89d40bc3af4
yarnnys_home <yarnnyarmy@gmail.com>
2025-11-29 08:43:59 -0500

    Fixed storing doublebottomW table

--- NinjaScript_Tables.sql
+++ NinjaScript_Tables.sql
@@ -12,24 +12,24 @@ EXEC dbo.GetLastDoubleBottomW;
 TRUNCATE TABLE dbo.DoubleBottomW;
 */
 
-DROP TABLE IF EXISTS DoubleBottomW;
-CREATE TABLE DoubleBottomW
-(
-    Id INT IDENTITY(1,1) PRIMARY KEY,   -- unique key for each record
+ALTER TABLE dbo.DoubleBottomW
+ADD PictureFile NVARCHAR(512) NULL;
 
+DROP TABLE IF EXISTS DoubleBottomW;
+CREATE TABLE dbo.DoubleBottomW
+(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
     FirstLowBarDate   DATETIME      NOT NULL,
     FirstLowBarPrice  DECIMAL(18,6) NOT NULL,
     FirstLowBarCount  INT           NOT NULL,
-
     SecondLowBarDate  DATETIME      NOT NULL,
     SecondLowBarPrice DECIMAL(18,6) NOT NULL,
     SecondLowBarCount INT           NOT NULL,
-
     HighBarDate       DATETIME      NOT NULL,
     HighBarPrice      DECIMAL(18,6) NOT NULL,
     HighBarCount      INT           NOT NULL,
-
     LeftSideWTime     DATETIME      NOT NULL,
     LeftSideWPrice    DECIMAL(18,6) NOT NULL,
-    LeftSideWBarCount INT           NOT NULL
+    LeftSideWBarCount INT           NOT NULL,
+    PictureFile       NVARCHAR(512) NULL
 );
